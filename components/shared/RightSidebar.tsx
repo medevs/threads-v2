ce387c2b9c220d49a48b29255535f12c979abd24
Ahmed Oublihi <a.oublihi@ephilos.lokal>
2023-08-24 14:38:49 +0200

    Home Layout

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,43 @@
+import { currentUser } from "@clerk/nextjs";
+
+async function RightSidebar() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  return (
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>
+          Suggested Communities
+        </h3>
+
+        {/* <div className='mt-7 flex w-[350px] flex-col gap-9'>
+          {suggestedCOmmunities.communities.length > 0 ? (
+            <>
+              {suggestedCOmmunities.communities.map((community) => (
+                <UserCard
+                  key={community.id}
+                  id={community.id}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  personType='Community'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>
+              No communities yet
+            </p>
+          )}
+        </div> */}
+      </div>
+
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
+      </div>
+    </section>
+  );
+}
+
+export default RightSidebar;
