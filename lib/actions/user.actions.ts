4cc6c340e10b233d7eba4b233ee72cd4c3ab3318
Ahmed Oublihi <a.oublihi@ephilos.lokal>
2023-09-01 06:19:29 +0200

    profile

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,7 +4,7 @@ import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
 
 // import Community from "../models/community.model";
-// import Thread from "../models/thread.model";
+import Thread from "../models/thread.model";
 import User from "../models/user.model";
 
 import { connectToDB } from "../mongoose";
@@ -62,37 +62,37 @@ export async function updateUser({
   }
 }
 
-// export async function fetchUserPosts(userId: string) {
-//   try {
-//     connectToDB();
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
 
-//     // Find all threads authored by the user with the given userId
-//     const threads = await User.findOne({ id: userId }).populate({
-//       path: "threads",
-//       model: Thread,
-//       populate: [
-//         {
-//           path: "community",
-//           model: Community,
-//           select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
-//         },
-//         {
-//           path: "children",
-//           model: Thread,
-//           populate: {
-//             path: "author",
-//             model: User,
-//             select: "name image id", // Select the "name" and "_id" fields from the "User" model
-//           },
-//         },
-//       ],
-//     });
-//     return threads;
-//   } catch (error) {
-//     console.error("Error fetching user threads:", error);
-//     throw error;
-//   }
-// }
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        // {
+        //   path: "community",
+        //   model: Community,
+        //   select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+        // },
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id", // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user threads:", error);
+    throw error;
+  }
+}
 
 // Almost similar to Thread (search + pagination) and Community (search + pagination)
 export async function fetchUsers({
