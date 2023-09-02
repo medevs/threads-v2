5c1854113d8612694a658510c8ec999743940220
Ahmed Oublihi <a.oublihi@ephilos.lokal>
2023-09-02 06:55:45 +0200

    onbouarding fix

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,24 +1,24 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null; // to avoid typescript warnings
 
-  // const userInfo = await fetchUser(user.id);
-  // if (userInfo?.onboarded) redirect("/");
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
-  // const userData = {
-  //   id: user.id,
-  //   objectId: userInfo?._id,
-  //   username: userInfo ? userInfo?.username : user.username,
-  //   name: userInfo ? userInfo?.name : user.firstName ?? "",
-  //   bio: userInfo ? userInfo?.bio : "",
-  //   image: userInfo ? userInfo?.image : user.imageUrl,
-  // };
+  const userData = {
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
 
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import ThreadCard from "@/components/cards/ThreadCard";
 // import Pagination from "@/components/shared/Pagination";
 
 import { fetchPosts } from "@/lib/actions/thread.actions";
-// import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Home({
   searchParams,
@@ -15,8 +15,8 @@ async function Home({
   const user = await currentUser();
   if (!user) return null;
 
-  // const userInfo = await fetchUser(user.id);
-  // if (!userInfo?.onboarded) redirect("/onboarding");
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchPosts(
     searchParams.page ? +searchParams.page : 1,
